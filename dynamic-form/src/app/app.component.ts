8e2b9a419c8b6ea1966399a0cfaa48cd686af57c
TammireddiSrinivas <36499709+TammireddiSrinivas@users.noreply.github.com>
2019-05-16 15:07:27 +0530

    Update app.component.ts

--- dynamic-form/src/app/app.component.ts
+++ dynamic-form/src/app/app.component.ts
@@ -1,96 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
-  selector: 'my-app',
+
+  selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+
+  styleUrls: ['./app.component.css']
+
 })
-export class AppComponent  {
-  name = 'Angular 6';
+
+export class AppComponent implements OnInit {
+
+  title = 'CodeInfo';
+
+  constructor(private _http: HttpClient) {
+  }
+
   formName = 'Registration';
-  formJson:FormTypes[] =[
-    {
-      "label":"Username",
-      "tag":"input",
-      "type":"text",
-      "placeholder":"enter name",
-      value:"Srinivas",
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },
-    {
-      "label":"Email Id",
-      "tag":"input",
-      "type":"email",
-      "placeholder":"please enter email",
-      value:"sri@gmail.com",
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },
-    {
-      "label":"Password",
-      "tag":"input",
-      "type":"password",
-      "placeholder":"please enter your password",
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },
-    {
-      "label":"Mobile Number",
-      "tag":"input",
-      "type":"tel",
-      "placeholder":"enter mobile number",
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },   
-    {
-      "label":"Gender",
-      "tag":"input",
-      "type":"radio",
-      "name":"gender",
-      "placeholder":"enter name",
-      values:['male','female'],
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },
-    {
-      tag:'select',
-      values:['1','2','3','4'],
-      label:'user choice',
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-    },
-    {
-      tag:'input',
-      type:'checkbox',
-      label:'select courses',
-      values:['java','c','.net'],
-      "lableStyles":{'font-weight':'bold'},
-      "inputStyles":{'background-color':'#d2d8e0','color':'black'}
-              
-    },
-    {
-      "value":"cancel",
-      "tag":"button"
-    },
-    {
-      "type":"submit",
-      "value":"submit",
-      "tag":"button",
-      "buttonStyle":{'background-color':'#26b536'}
-    }
-  ]
+
+  formJson: FormTypes[];
+
+  ngOnInit(): void {
+
+      this.getFormInfo();
+
+  }
+
+
+
+  getFormInfo() {
+
+    this._http.get('assets/formdata.json').subscribe((res: any) => {
+
+      this.formJson = res;
+
+    }, error => {
+
+      console.log(error);
+
+    });
+
+  }
 }
 
-export interface FormTypes{
-  label?:string;
-  tag:string;
-  value?:string;
-  type?:string;
-  placeholder?:string;
-  values?:any;
-  name?:string;
-  lableStyles?:any;
-  inputStyles?:any;
-  buttonStyle?:any;
+
+export interface FormTypes {
+
+  label?: string;
+
+  tag: string;
+
+  value?: string;
+
+  type?: string;
+
+  placeholder?: string;
+
+  values?: any;
+
+  name?: string;
+
+  inputStyles?: any;
+
+  labelStyles?: any;
+
 }
+
